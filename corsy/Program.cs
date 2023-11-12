7d76dad723d8348a28805fc24c9eaaf4987e0ce5
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2023-11-12 23:32:25 +0100

    added post path + exported logic to handler method

--- corsy/Program.cs
+++ corsy/Program.cs
@@ -1,11 +1,20 @@
+using System;
+using System.Collections;
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Mime;
+using System.Reflection.PortableExecutable;
 using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Extensions.Primitives;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace corsy
 {
@@ -30,10 +39,9 @@ namespace corsy
             {
                 return new HttpClientHandler()
                 {
-                    Proxy = new WebProxy()
+                    Proxy = new WebProxy(Environment.GetEnvironmentVariable("ProxyAddress") ??
+                                         builder.Configuration["ProxyAddress"])
                     {
-                        Address = new Uri(Environment.GetEnvironmentVariable("ProxyAddress") ??
-                                          builder.Configuration["ProxyAddress"]),
                         Credentials = new NetworkCredential(
                             Environment.GetEnvironmentVariable("ProxyUser") ?? builder.Configuration["ProxyUser"],
                             Environment.GetEnvironmentVariable("ProxyUserPassword") ??
@@ -49,68 +57,108 @@ namespace corsy
             // Configure the HTTP request pipeline.
 
             app.UseHttpsRedirection();
-           
+
             app.UseHeaderPropagation();
 
             app.UseAuthorization();
 
+            var omittedHeaders = builder.Configuration.GetSection("omitHeaders").Get<string[]>() ?? new string[] { };
+            
+            app.MapGet("/corsy/{*uri}", async Task<IResult> (HttpContext httpContext, IHttpClientFactory httpClientFactory, string? uri) 
+                => await CorsyHandler(httpContext, httpClientFactory, omittedHeaders, uri));
 
-            app.Map("/corsy/{*uri}", async Task<IResult> (HttpContext httpContext, IHttpClientFactory httpClientFactory, string? uri) =>
-            {
-                if (String.IsNullOrEmpty(uri))
-                {
-                    return Results.BadRequest("Pass url as a parameter!");
-                }
-
-                var client = httpClientFactory.CreateClient("proxyHttp");
-                var omittedHeaders = builder.Configuration.GetSection("omitHeaders").Get<string[]>() ?? new string[] {};
-                httpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
-                if (Uri.TryCreate(uri, UriKind.Absolute, out _))
-                {
-                    try
-                    {
-
-                        var req = new HttpRequestMessage()
-                        {
-                            RequestUri = new Uri(uri+ httpContext.Request.QueryString),
-                            Method = new HttpMethod(httpContext.Request.Method),
-                        };
-                        foreach (var header in httpContext.Request.Headers.Where(x => x.Value != StringValues.Empty))
-                        {
-                            if (header.Key != "Host" && !omittedHeaders.Contains(header.Key))
-                            {
-                                req.Headers.Add(header.Key, values: header.Value);
-                            }
-                            
-                        }
-
-                        var response = await client.SendAsync(req);
-
-
-                        // var response = await client.GetAsync(uri);
-
-
-                        if (response.IsSuccessStatusCode)
-                        {
-                            return Results.Stream(await response.Content.ReadAsStreamAsync(), response.Content.Headers.ContentType?.ToString());
-                        }
-
-                        return Results.Problem(statusCode: (int) response.StatusCode, detail: response.ReasonPhrase);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Error: {0}",e.Message);
-                        //throw;
-                        return Results.Problem();
-                    }
-
-                }
-
-                return Results.BadRequest("Bad url!");
-
-            });
+            app.MapPost("/corsy/{method:alpha}/{*uri}", async Task<IResult> (HttpContext httpContext, IHttpClientFactory httpClientFactory, string? uri, string method,[FromBody(EmptyBodyBehavior = EmptyBodyBehavior.Disallow)] IDictionary<string, string> specialHeaders) 
+                => await CorsyHandler(httpContext, httpClientFactory, omittedHeaders, uri,method, specialHeaders));
 
             app.Run();
+
         }
+
+        public static async Task<IResult> CorsyHandler(HttpContext httpContext, IHttpClientFactory httpClientFactory, IEnumerable<string> omittedHeaders, string? uri,string method = "get", IDictionary<string, string>? specialHeaders = null)
+        {
+            var allOmittedHeaders = omittedHeaders.ToList();
+
+            if (String.IsNullOrEmpty(uri))
+            {
+                return Results.BadRequest("Pass url as a parameter!");
+            }
+
+            if (specialHeaders is null)
+            {
+                //return Results.BadRequest("Pass headers you want to add!");
+                specialHeaders = new Dictionary<string, string>();
+            }
+
+
+            //lazy but works
+            try
+            {
+                var d = new HttpMethod(method);
+            }
+            catch (Exception e)
+            {
+                return Results.BadRequest("Bad request method!");
+            }
+
+
+            if (method == "get")
+            {
+                 allOmittedHeaders.AddRange(new[] { "Content-Type", "Content-Length"});
+            }
+
+            var client = httpClientFactory.CreateClient("proxyHttp");
+            httpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            if (Uri.TryCreate(uri, UriKind.Absolute, out _))
+            {
+                try
+                {
+
+                    var req = new HttpRequestMessage()
+                    {
+                        RequestUri = new Uri(uri + httpContext.Request.QueryString),
+                        Method = new HttpMethod(method),
+                    };
+
+                    foreach (var specialHeader in specialHeaders)
+                    {
+                        req.Headers.Add(specialHeader.Key, value: specialHeader.Value);
+                    }
+
+
+
+                    foreach (var header in httpContext.Request.Headers.Where(x => x.Value != StringValues.Empty))
+                    {
+                        if (!allOmittedHeaders.Contains(header.Key))
+                        {
+                            req.Headers.Add(header.Key, values: header.Value);
+                        }
+
+                    }
+
+                    var response = await client.SendAsync(req);
+
+
+                    // var response = await client.GetAsync(uri);
+
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return Results.Stream(await response.Content.ReadAsStreamAsync(), response.Content.Headers.ContentType?.ToString());
+                    }
+
+                    return Results.Problem(statusCode: (int)response.StatusCode, detail: response.ReasonPhrase);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: {0}", e.Message);
+                    //throw;
+                    return Results.Problem();
+                }
+
+            }
+
+            return Results.BadRequest("Bad url!");
+        }
+
     }
 }
